069edc41f9716e3699faf0333f64d93e3021f5b8
Muhammad-Raiyan <mislam0525@gmail.com>
2016-07-15 01:06:51 -0400

    Change in IO, added args and try-catch

--- Project1-CS313/src/Project1.java
+++ Project1-CS313/src/Project1.java
@@ -3,71 +3,75 @@ import java.util.*;
 
 public class Project1 {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		Polynomials pol1 = new Polynomials();
 		Polynomials pol2 = new Polynomials();
-				
-		
-		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
-		
-		String line;
-		
-		do{
-			line = br.readLine();
-			if(line==null) break;
-			
-			if(!line.isEmpty()){
-				
-				pol1 = store(line);
-				
-				line = br.readLine();
-				pol2 = store(line);				
+
+		/*
+		 * IO - Read from file as String, split it after each space, parse it to
+		 * int and add it to polynomial
+		 */
+		if (args.length == 0)
+			System.out.println("No file specified");
+		else {
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(args[0]));
+				String line;
+
+				do {
+					if ((line = br.readLine()) == null)
+						break;
+
+					if (!line.isEmpty()) {
+
+						pol1 = store(line);
+
+						line = br.readLine();
+						pol2 = store(line);
+					} else {
+						System.out.print("Empty Line");
+					}
+					System.out.println(pol1.toString());
+					System.out.println(pol2.toString());
+
+					pol1.makeEmpty();
+					pol2.makeEmpty();
+
+				} while (line != null);
+
+				br.close();
+			} catch (Exception e) {
+				System.out.println(e);
 			}
-			else 
-				System.out.println("Empty Line");
-			
-//			while(!pol1.isEmpty()){
-//				System.out.println( pol1.removeLast() );
-//			}
-			System.out.println(pol1.toString());
-			System.out.println(pol2.toString());
-			
-			pol1.makeEmpty();
-			pol2.makeEmpty();
-			
-		}while(line!=null);
-		
-		//String everything = sb.toString();
-		br.close();
-		
-		
-		//System.out.println(everything);
-		
-		/*System.out.println("Enter a string: ");
-		System.out.flush();
-		
-		while((c=System.in.read())!='\r') pol1.addLast(new Integer(c));
-		*/
-		/*System.out.println("Input string reversed");
-		while(!pol1.isEmpty()){
-			System.out.println( (char)(((Integer)pol1.removeLast()).intValue()) );
-		}*/
+		} // End of IO
+
+		// System.out.println(everything);
+
+		/*
+		 * System.out.println("Enter a string: "); System.out.flush();
+		 * 
+		 * while((c=System.in.read())!='\r') pol1.addLast(new Integer(c));
+		 */
+		/*
+		 * System.out.println("Input string reversed"); while(!pol1.isEmpty()){
+		 * System.out.println( (char)(((Integer)pol1.removeLast()).intValue())
+		 * ); }
+		 */
 
 	}
-	
-	public static Polynomials store(String s){
+
+	public static Polynomials store(String s) {
 		Polynomials p = new Polynomials();
-		
+
 		String[] temp_pol = s.split(" ");
-		for(String elements: temp_pol){
+		for (String elements : temp_pol) {
 			p.addLast(Integer.parseInt(elements));
 		}
 		return p;
 	}
-	
-	public static void sum(Polynomials p1, Polynomials p2){
-		
-		
+
+	public static void sum(Polynomials p1, Polynomials p2) {
+
 	}
 
 }
