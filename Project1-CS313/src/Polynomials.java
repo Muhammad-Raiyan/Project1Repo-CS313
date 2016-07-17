791bffcc92bf9b4b613497b7a7229989fe40638c
Muhammad-Raiyan <mislam0525@gmail.com>
2016-07-16 21:46:59 -0400

    Created functional sum(), sumWith(), getIterator(), Polynomials(Polynomials) and declared product()

--- Project1-CS313/src/Polynomials.java
+++ Project1-CS313/src/Polynomials.java
@@ -3,45 +3,65 @@ public class Polynomials {
 
 	private Node head;
 	private int size;
-	
-	public Polynomials(){
+
+	public Polynomials() {
 		head = null;
 		size = 0;
 	}
-	public Polynomials(int x){
-		head=new Node(x);
-		size++;
+
+	public Polynomials(Polynomials p) {
+		Node it = p.getIterator();
+		while (it != null) {
+			this.addLast(it.getElement());
+			it = it.getNext();
+		}
 	}
-	
+
+	public Polynomials(int x) {
+		/*
+		 * head = new Node(x); size++;
+		 */
+		this.addLast(x);
+	}
+
 	public int size() {
 		return size;
 	}
-	
+
 	public boolean isEmpty() {
 		return head == null;
 	}
-	
+
 	public void makeEmpty() {
 		head = null;
 		size = 0;
 	}
-	
+
+	public Node getIterator() {
+		if (head == null)
+			throw new RuntimeException("Head in getIterator is empty");
+		Node current = head;
+		return current;
+	}
+
 	public void addLast(int x) {
-		if(head==null){
-			head=new Node(x);
+		if (head == null) {
+			head = new Node(x);
 			size++;
-			return;
+		} else {
+			Node n = new Node(x);
+			Node current = head;
+			while (current.getNext() != null) {
+				current = current.getNext();
+			}
+			current.setNext(n);
+			size++;
 		}
-		Node n = new Node(x);
-		Node current=head;
-		while(current.getNext()!=null){
-			current=current.getNext();
-		}
-		current.setNext(n);
-		size++;
 	}
 
 	public int removeLast() {
+		if (head == null)
+			throw new RuntimeException("The LinkedList is Empty. Call from removeLast().");
 		int temp;
 		temp = head.getElement();
 		head = head.getNext();
@@ -49,20 +69,37 @@ public class Polynomials {
 		return temp;
 	}
 
-	public int last() {		
+	public int last() {
+		if (head == null)
+			throw new RuntimeException("The LinkedList is Empty. Call from last().");
 		Node current = head;
-		while(current.getNext()!=null){
+		while (current.getNext() != null) {
 			current = current.getNext();
 		}
 		return current.getElement();
-		
+
 	}
-	
-	@Override
-	public String toString(){
-		StringBuilder out = new StringBuilder();
+
+	public Polynomials sumWith(Polynomials p) {
+		if (head == null)
+			throw new RuntimeException("The LinkedList is Empty. Call from sumWith().");
 		Node current = head;
-		while(current!=null){
+		Node it = p.getIterator();
+		while (current != null) {
+			current.setElement(current.getElement() + it.getElement());
+			current = current.getNext();
+			it = it.getNext();
+		}
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder out = new StringBuilder();
+		if (head == null)
+			throw new RuntimeException("The LinkedList is Empty. Call from toString().");
+		Node current = head;
+		while (current != null) {
 			out.append(current.getElement());
 			out.append(" ");
 			current = current.getNext();
--- Project1-CS313/src/Project1.java
+++ Project1-CS313/src/Project1.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Project1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Polynomials pol1 = new Polynomials();
 		Polynomials pol2 = new Polynomials();
 
@@ -14,38 +14,41 @@ public class Project1 {
 		if (args.length == 0)
 			System.out.println("No file specified");
 		else {
-			try {
-				BufferedReader br = new BufferedReader(new FileReader(args[0]));
-				String line;
 
-				do {
-					if ((line = br.readLine()) == null)
-						break;
+			BufferedReader br = new BufferedReader(new FileReader(args[0]));
+			String line;
 
-					if (!line.isEmpty()) {
+			do {
 
-						pol1 = store(line);
-
-						line = br.readLine();
-						pol2 = store(line);
-					} else {
-						System.out.print("Empty Line");
-					}
-					System.out.println(pol1.toString());
-					System.out.println(pol2.toString());
+				if ((line = br.readLine()) == null || line.isEmpty()) {
+					System.out.println("The sum of the 2 polynomials is: ");
+					System.out.println(sum(pol1, pol2).toString());
+					System.out.println("The product of the two polynomials is: ");
+					System.out.println(product(pol1, pol2).toString());
 
 					pol1.makeEmpty();
 					pol2.makeEmpty();
+				}
 
-				} while (line != null);
+				else {
 
-				br.close();
-			} catch (Exception e) {
-				System.out.println(e);
-			}
+					pol1 = store(line);
+
+					line = br.readLine();
+					pol2 = store(line);
+				}
+				/*
+				 * System.out.println(pol1.toString());
+				 * System.out.println(pol1.size());
+				 * System.out.println(pol2.toString());
+				 * System.out.println(pol1.size());
+				 */
+
+			} while (line != null);
+
+			br.close();
 		} // End of IO
-
-		// System.out.println(everything);
+			// System.out.println(everything);
 
 		/*
 		 * System.out.println("Enter a string: "); System.out.flush();
@@ -57,7 +60,6 @@ public class Project1 {
 		 * System.out.println( (char)(((Integer)pol1.removeLast()).intValue())
 		 * ); }
 		 */
-
 	}
 
 	public static Polynomials store(String s) {
@@ -70,8 +72,27 @@ public class Project1 {
 		return p;
 	}
 
-	public static void sum(Polynomials p1, Polynomials p2) {
+	public static Polynomials sum(Polynomials p1, Polynomials p2) {
 
+		Polynomials temp1 = new Polynomials(p1);
+		Polynomials temp2 = new Polynomials(p2);
+
+		int diff = temp1.size() - temp2.size();
+
+		if (diff > 0) {
+			for (int i = 0; i < diff; i++)
+				temp2.addLast(0);
+		}
+		if (diff < 0) {
+			for (int i = 0; i < Math.abs(diff); i++)
+				temp1.addLast(0);
+		}
+		return temp1.sumWith(temp2);
+	}
+
+	public static Polynomials product(Polynomials p1, Polynomials p2) {
+
+		return p1;
 	}
 
 }
